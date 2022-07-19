a2cd691ca74a0e2e1afb092f24e5f1ccbd6d3590
amruthbtc <amruthap@boston-technology.com>
2022-07-19 20:08:35 +0530

    issue fixes: #4703 #4681 #4713 #4717 #4714

--- iOS/MyStudies/MyStudies/Networking/Network setup/NetworkWebServiceHandler.swift
+++ iOS/MyStudies/MyStudies/Networking/Network setup/NetworkWebServiceHandler.swift
@@ -454,7 +454,7 @@ class NetworkWebServiceHandler: NSObject, URLSessionDelegate {
           responseDict =
             try JSONSerialization.jsonObject(with: data!, options: [])
             as? NSDictionary
-          print("4response---\(requestName)---\(responseDict)")
+//          print("4response---\(requestName)---\(responseDict)")
           
         } catch let error {
           Logger.sharedInstance.error("Serialization error: \(requestName ?? "")", error.localizedDescription)
